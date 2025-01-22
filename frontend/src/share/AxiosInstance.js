a772eaf6debb29c39ef898b514c0022c83e534dd
Ted Herambert <tedherambert2004@gmail.com>
2025-01-22 15:13:12 +0100

    add json-server-version branch to repo

--- frontend/src/share/AxiosInstance.js
+++ frontend/src/share/AxiosInstance.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-// backend
-// const Axios = axios.create({ baseURL: 'http://localhost:3000', withCredentials: true });
-// using json server
 const Axios = axios.create({ baseURL: 'http://localhost:5000', withCredentials: false });
 export default Axios;
+
+// json-server -> json-server --watch db.json --port 5000
