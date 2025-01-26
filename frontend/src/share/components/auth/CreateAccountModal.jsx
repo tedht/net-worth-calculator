2f50df8cca81e3079a11e6edf483f94a7c8f31d7
Ted Herambert <tedherambert2004@gmail.com>
2025-01-26 21:35:25 +0100

    current v

--- frontend/src/share/components/auth/CreateAccountModal.jsx
+++ frontend/src/share/components/auth/CreateAccountModal.jsx
@@ -1,11 +1,11 @@
 import { useState, useContext } from "react";
-import { Modal, Card, CardContent, Box, Typography, IconButton, TextField, Button, Checkbox, FormControl, FormHelperText } from "@mui/material";
+import { Box, Typography, TextField, Button, Checkbox, FormControl, FormHelperText } from "@mui/material";
 import GlobalContext from '../../GlobalContext';
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-import { useTheme } from '@mui/material';
 
 import { useMutation } from 'react-query';
 import Axios from "../../AxiosInstance";
+
+import ModalCard from "../ModalCard";
 
 import dayjs from 'dayjs';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
@@ -18,20 +18,20 @@ const CreateAccountModal = ({
 	handleCloseCreateAccount = () => {},
 	handleOpenLogin = () => {}
 }) => {
-	const [firstname,        setFirstname]        = useState('');
-	const [firstnameError,   setFirstnameError]   = useState('');
-	const [lastname,         setLastname]         = useState('');
-	const [lastnameError,    setLastnameError]    = useState('');
-	const [email,            setEmail]            = useState('');
-	const [emailError,       setEmailError]       = useState('');
-	const [password,         setPassword]         = useState('');
-	const [passwordError,    setPasswordError]    = useState('');
-	const [rePassword,       setRePassword]       = useState('');
-	const [rePasswordError,  setRePasswordError]  = useState('');
+	const [firstname,        setFirstname       ] = useState('');
+	const [firstnameError,   setFirstnameError  ] = useState('');
+	const [lastname,         setLastname        ] = useState('');
+	const [lastnameError,    setLastnameError   ] = useState('');
+	const [email,            setEmail           ] = useState('');
+	const [emailError,       setEmailError      ] = useState('');
+	const [password,         setPassword        ] = useState('');
+	const [passwordError,    setPasswordError   ] = useState('');
+	const [rePassword,       setRePassword      ] = useState('');
+	const [rePasswordError,  setRePasswordError ] = useState('');
 	const [inputDateOfBirth, setInputDateOfBirth] = useState(dayjs());
-	const [dateOfBirth,      setDateOfBirth]      = useState(dayjs().format('YYYY-MM-DD'));
-	const [checked,          setChecked]          = useState(false);
-	const [checkedError,     setCheckedError]     = useState('');
+	const [dateOfBirth,      setDateOfBirth     ] = useState(dayjs().format('YYYY-MM-DD'));
+	const [checked,          setChecked         ] = useState(false);
+	const [checkedError,     setCheckedError    ] = useState('');
 
 	const { setUser } = useContext(GlobalContext);
 
@@ -56,8 +56,8 @@ const CreateAccountModal = ({
 				resetAndClose();
 			},
 			onError: (error) => {
-				alert("Account creation error")
 				console.error(error);
+				alert("Account creation error")
 				setPassword('');
 				setRePassword('');
 			},
@@ -150,154 +150,106 @@ const CreateAccountModal = ({
 		return isValid;
 	};
 
-	const theme = useTheme();
-	
-	const modalCardStyle = {
-		position: 'absolute',
-		left: '50%',
-		transform: 'translate(-50%, 0%)',
-		width: { xs: '90%', md: '60%'},
-		border: 2,
-		borderRadius: 5,
-		mt: 2,
-		boxShadow: theme.shadows[10]
-	}
-
-	const modalHeaderStyle = { 
-		p: 1,  
-		background: theme.palette.primary.main,
-		display: 'flex', 
-		justifyContent: 'space-between' 
-	}
-
 	return (
-		<Modal 
-			open={open}
-			onClose={resetAndClose}
-			sx={{overflow: 'scroll'}}>
-			<Card sx={modalCardStyle}>
-				<CardContent sx={{ p: 0 }}>
+		<ModalCard open={open} title={'Create Account'} handleClose={resetAndClose}>
 
-					{/****************/}
-					{/* MODAL HEADER */}
-					{/****************/}
-					<Box borderBottom={2} sx={modalHeaderStyle}>
-						<Typography variant="h6" color="secondary">
-							Create Account
-						</Typography>
+		{/* Firstname */}
+		<TextField							
+			value={firstname}
+			onChange={(e) => setFirstname(e.target.value)}
+			fullWidth
+			error={firstnameError !== ''}
+			helperText={firstnameError}
+			label="First name"
+			placeholder="Type your first name"
+		/>
+			
+		{/* Lastname */}
+		<TextField							
+			value={lastname}
+			onChange={(e) => setLastname(e.target.value)}
+			fullWidth
+			error={lastnameError !== ''}
+			helperText={lastnameError}
+			label="Last name"
+			placeholder="Type your last name"
+		/>
+		
+		{/* Email */}
+		<TextField							
+			value={email}
+			onChange={(e) => setEmail(e.target.value)}
+			fullWidth
+			error={emailError !== ''}
+			helperText={emailError}
+			label="Email"
+			placeholder="Type your email"
+		/>
 
-						<IconButton onClick={resetAndClose}>
-							<CloseRoundedIcon color="contrastText"/>
-						</IconButton>
-					</Box>
+		{/* Date of birth */}
+		<LocalizationProvider dateAdapter={AdapterDayjs}>
+			<DemoContainer components={['DatePicker', 'DatePicker']}>
+				<DatePicker
+					label="Date of birth"
+					value={inputDateOfBirth}
+					onChange={(e) => handleNewDateOfBirth(e)}/>
+			</DemoContainer>
+		</LocalizationProvider>
 
-					{/****************/}
-					{/*  MODAL BODY  */}
-					{/****************/}
-					<Box sx={{
-						p: 2, 
-						gap: 2, 
-						display: 'flex', 
-						flexDirection: 'column', 
-						alignItems: 'center'}}>
+		{/* Password */}
+		<TextField							
+			value={password}
+			onChange={(e) => setPassword(e.target.value)}
+			fullWidth
+			error={passwordError !== ''}
+			helperText={passwordError}
+			type="password"
+			label="Password"
+			placeholder="Type your password"
+		/>
 
-						{/* Firstname */}
-						<TextField							
-							value={firstname}
-							onChange={(e) => setFirstname(e.target.value)}
-							fullWidth
-							error={firstnameError !== ''}
-							helperText={firstnameError}
-							label="First name"
-							placeholder="Type your first name"
-						/>
-							
-						{/* Lastname */}
-						<TextField							
-							value={lastname}
-							onChange={(e) => setLastname(e.target.value)}
-							fullWidth
-							error={lastnameError !== ''}
-							helperText={lastnameError}
-							label="Last name"
-							placeholder="Type your last name"
-						/>
-						
-						{/* Email */}
-						<TextField							
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-							fullWidth
-							error={emailError !== ''}
-							helperText={emailError}
-							label="Email"
-							placeholder="Type your email"
-						/>
+		{/* RePassword */}
+		<TextField							
+			value={rePassword}
+			onChange={(e) => setRePassword(e.target.value)}
+			fullWidth
+			error={rePasswordError !== ''}
+			helperText={rePasswordError}
+			type="password"
+			label="Confirm password"
+			placeholder="Type your password"
+		/>
 
-						<LocalizationProvider dateAdapter={AdapterDayjs}>
-							<DemoContainer components={['DatePicker', 'DatePicker']}>
-								<DatePicker
-									label="Date of birth"
-									value={inputDateOfBirth}
-									onChange={(e) => handleNewDateOfBirth(e)}/>
-							</DemoContainer>
-						</LocalizationProvider>
+		{/* Privacy policy */}
+		<FormControl error={!!checkedError}>
+			<Box display={'flex'}>
+				<Checkbox	 
+					checked={checked}
+					onChange={handleCheck}/>
+				<Typography variant="body1" alignItems={'center'} display={'flex'}>
+					I have read and agree to the privacy policy of this website.
+				</Typography>
+			</Box>
 
-						<TextField							
-							value={password}
-							onChange={(e) => setPassword(e.target.value)}
-							fullWidth
-							error={passwordError !== ''}
-							helperText={passwordError}
-							type="password"
-							label="Password"
-							placeholder="Type your password"
-						/>
+			<FormHelperText>
+				{(!!checkedError) ? checkedError : ''}
+			</FormHelperText>
+		</FormControl>
 
-						<TextField							
-							value={rePassword}
-							onChange={(e) => setRePassword(e.target.value)}
-							fullWidth
-							error={rePasswordError !== ''}
-							helperText={rePasswordError}
-							type="password"
-							label="Confirm password"
-							placeholder="Type your password"
-						/>
+		<Button 
+			variant="contained" 
+			color="primary"
+			onClick={handleSubmit}>
+			Submit
+		</Button>
 
-						<FormControl error={!!checkedError}>
-
-							<Box display={'flex'}>
-								<Checkbox	 
-									checked={checked}
-									onChange={handleCheck}
-								/>
-								<Typography variant="body1" alignItems={'center'} display={'flex'}>
-									I have read and agree to the privacy policy of this website.
-								</Typography>
-							</Box>
-
-							<FormHelperText>
-								{(!!checkedError) ? checkedError : ''}
-							</FormHelperText>
-						</FormControl>
-
-						<Button 
-							variant="contained" 
-							color="primary"
-							onClick={handleSubmit}>
-							Create Account
-						</Button>
-
-						<Button 
-							variant="text" 
-							onClick={() => {handleCloseCreateAccount(); handleOpenLogin();}}>
-							Already have an account ?
-						</Button>
-					</Box>
-				</CardContent>
-			</Card>
-		</Modal>
+		<Button 
+			variant="text" 
+			onClick={() => {handleCloseCreateAccount(); handleOpenLogin();}}>
+			Already have an account ?
+		</Button>
+		
+		</ModalCard>
 	);
 }
 
